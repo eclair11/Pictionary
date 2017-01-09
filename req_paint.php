b0c096ca23e20d29f92c1a79a786e69155e94c20
Elias Romdan <elias.romdan@etu.unice.fr>
2017-01-09 09:31:55 +0100

    Synchro avec la BD

--- req_paint.php
+++ req_paint.php
@@ -3,9 +3,6 @@ session_start();
 $id = $_SESSION["id"];
 $commands = $_POST["drawingCommands"];
 $picture = $_POST["picture"];
-echo($id);
-echo($commands);
-echo($picture);
 $dbh = new PDO('mysql:host=localhost;dbname=pictionnary', 'test', 'test');
 
-$sql = $dbh->query("INSERT INTO drawings(id,commands,picture) VALUES ('$id','$commands','$picture')");
+$sql = $dbh->query("INSERT INTO drawings(id,commands,picture) VALUES ($id,'$commands','$picture')");
