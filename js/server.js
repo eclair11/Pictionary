5f634adf98f0f967c0fa0b3d90a03ba7c19beeb8
Elias Romdan <elias.romdan@etu.unice.fr>
2017-01-23 01:58:01 +0100

    Ajout du logout

--- js/server.js
+++ js/server.js
@@ -19,8 +19,8 @@ function RequestCheckSQL(username, password, res) {
     connection.query("SELECT * FROM users WHERE email='"+username+"' AND password='"+password+"'",
         function (err, rows, fields) {
         if(!err)
-            if (rows.length>0) {
-                session.start = true;
+            if (rows.length > 0) {
+                session.id = rows[0].id;
                 session.nom = rows[0].nom;
                 session.prenom = rows[0].prenom;
                 session.profilepic = rows[0].profilepic;
@@ -34,22 +34,37 @@ function RequestCheckSQL(username, password, res) {
     });
 }
 
-function RequestInsertSQL(info, res) {
+function RequestSubscribeSQL(info, res) {
     connection.query("INSERT INTO users" +
         "(email,password,nom,prenom,tel,website,sexe,birthdate,ville,taille,couleur,profilepic)" +
         "VALUES ('"+info.email+"','"+info.password+"','"+info.nom+"','"+info.prenom+"','"+info.tel+"','" +
         ""+info.website+"','"+info.sexe+"','"+info.birthdate+"','"+info.ville+"','"+info.taille+"','" +
         info.couleur+"','"+info.profilepic+"')", function (err, rows, fields) {
         if(!err) {
-            session.start = true;
-            session.nom = info.nom;
-            session.prenom = info.prenom;
-            session.profilepic = info.profilepic;
-            session.couleur = info.couleur;
-            res.redirect('/profile');
+            connection.query("SELECT * FROM users WHERE email='"+info.email+"' AND password='"+info.password+"'",
+                function (err, rows, fields) {
+                session.id = rows[0].id;
+                session.nom = rows[0].nom;
+                session.prenom = rows[0].prenom;
+                session.profilepic = rows[0].profilepic;
+                session.couleur = rows[0].couleur;
+                res.redirect('/profile');
+            });
         }
-        else
+        else {
+            logger.error(err);
             res.redirect('/inscription');
+        }
+    });
+}
+
+function RecordDraw(id, commands, picture, res) {
+    connection.query("INSERT INTO drawings (id,commands,picture) VALUES" +
+        "("+id+",'"+commands+"','"+picture+"')", function (err, rows, fields) {
+        if(!err)
+            res.redirect('/profile');
+        else
+            logger.error(err);
     });
 }
 
@@ -63,10 +78,10 @@ app.get('/login', function(req, res){
     res.render('login');
 });
 
-app.post('/login', function (req, res) {
+app.post('/login', function(req, res) {
     var username = req.body.username;
     var password = req.body.password;
-    if (username != "" && password != "")
+    if (username != null && password != null)
         RequestCheckSQL(username, password, res);
     else
         res.redirect('/login');
@@ -76,7 +91,7 @@ app.get('/inscription', function(req, res){
     res.render('inscription');
 });
 
-app.post('/inscription', function (req, res) {
+app.post('/inscription', function(req, res) {
 	var email = req.body.email;
 	var password = req.body.password;
 	var nom = req.body.nom;
@@ -89,22 +104,47 @@ app.post('/inscription', function (req, res) {
 	var taille = req.body.taille;
 	var couleur = req.body.couleur;
 	var profilepic = req.body.profilepic;
-	if (sexe == "")
+	if (sexe == null)
 	    sexe = "T";
 	var info = {email:email, password:password, nom:nom, prenom:prenom, tel:tel,
 		website:website, sexe:sexe, birthdate:birthdate, ville:ville, taille:taille,
 		couleur:couleur.substr(1,6), profilepic:profilepic};
-	RequestInsertSQL(info, res);
+    RequestSubscribeSQL(info, res);
 });
 
 app.get('/paint', function(req, res){
-    res.render('paint',{start:session.start, couleur:session.couleur});
+    res.render('paint',{id:session.id, couleur:session.couleur});
 });
 
-app.post('/paint', function (req, res) {
-
+app.post('/paint', function(req, res) {
+    var id = session.id;
+    var commands = req.body.drawingCommands;
+    var picture = req.body.picture;
+    RecordDraw(id, commands, picture, res);
 });
 
 app.get('/profile', function(req, res){
-    res.render('profile',{start:session.start, nom:session.nom, prenom:session.prenom, profilepic:session.profilepic});
+    res.render('profile',{id:session.id, nom:session.nom, prenom:session.prenom, profilepic:session.profilepic});
+});
+
+app.get('/guess', function(req, res){
+    connection.query("SELECT commands FROM drawings WHERE id='"+session.id+"'", function (err, rows, fields) {
+        if (!err)
+            if (rows.length > 0) {
+                session.commands = rows[0].commands;
+                res.render('guess', {commands: session.commands});
+            }
+        else
+            logger.error(err);
+    });
+});
+
+app.get('/logout', function(req, res){
+    session.id = null;
+    session.nom = null;
+    session.prenom = null;
+    session.profilepic = null;
+    session.couleur = null;
+    session.commands = null;
+    res.render('logout');
 });
